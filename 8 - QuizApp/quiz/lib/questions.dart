1d029ae343fa93749f1764e52961cff301acd25b
caioagralemos <caioagralemos@live.com>
2023-07-13 17:07:32 -0300

    Update

--- 8 - QuizApp/quiz/lib/questions.dart
+++ 8 - QuizApp/quiz/lib/questions.dart
@@ -21,4 +21,92 @@ var questions = [
     ],
     'correctAnswer': 'c',
   },
+    {
+    'question': 'Qual é a capital do Brasil?',
+    'answers': [
+      {"a": "São Paulo"},
+      {"b": "Brasília"},
+      {"c": "Rio de Janeiro"},
+      {"d": "Belo Horizonte"},
+      {"e": "Salvador"},
+    ],
+    'correctAnswer': 'b',
+  },
+  {
+    'question': 'Quem escreveu a obra "Dom Casmurro"?',
+    'answers': [
+      {"a": "Machado de Assis"},
+      {"b": "Carlos Drummond de Andrade"},
+      {"c": "Clarice Lispector"},
+      {"d": "Jorge Amado"},
+      {"e": "Fernando Pessoa"},
+    ],
+    'correctAnswer': 'a',
+  },
+    {
+    'question': 'Quem pintou a Mona Lisa?',
+    'answers': [
+      {"a": "Vincent van Gogh"},
+      {"b": "Leonardo da Vinci"},
+      {"c": "Pablo Picasso"},
+      {"d": "Michelangelo"},
+      {"e": "Salvador Dalí"},
+    ],
+    'correctAnswer': 'b',
+  },
+  {
+    'question': 'Qual é o maior planeta do sistema solar?',
+    'answers': [
+      {"a": "Vênus"},
+      {"b": "Marte"},
+      {"c": "Júpiter"},
+      {"d": "Saturno"},
+      {"e": "Urano"},
+    ],
+    'correctAnswer': 'c',
+  },
+  {
+    'question': 'Quem escreveu a peça "Romeu e Julieta"?',
+    'answers': [
+      {"a": "William Shakespeare"},
+      {"b": "Oscar Wilde"},
+      {"c": "Friedrich Nietzsche"},
+      {"d": "Charles Dickens"},
+      {"e": "Jane Austen"},
+    ],
+    'correctAnswer': 'a',
+  },
+  {
+    'question': 'Qual é o rio mais longo do mundo?',
+    'answers': [
+      {"a": "Nilo"},
+      {"b": "Amazonas"},
+      {"c": "Mississippi"},
+      {"d": "Yangtzé"},
+      {"e": "Ganges"},
+    ],
+    'correctAnswer': 'a',
+  },
+  {
+    'question': 'Quem foi o primeiro presidente dos Estados Unidos?',
+    'answers': [
+      {"a": "George Washington"},
+      {"b": "Thomas Jefferson"},
+      {"c": "Abraham Lincoln"},
+      {"d": "John F. Kennedy"},
+      {"e": "Barack Obama"},
+    ],
+    'correctAnswer': 'a',
+  },
+  {
+    'question': 'Qual é o maior oceano do mundo?',
+    'answers': [
+      {"a": "Atlântico"},
+      {"b": "Pacífico"},
+      {"c": "Índico"},
+      {"d": "Ártico"},
+      {"e": "Antártico"},
+    ],
+    'correctAnswer': 'b',
+  },
 ];
